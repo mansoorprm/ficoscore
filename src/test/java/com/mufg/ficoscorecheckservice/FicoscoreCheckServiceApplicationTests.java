97b765e148e6a5cf605f68a28d134d7a037d0d03
Sumalatha Velma <sumalathav@virtusa.com>
2018-08-27 16:24:42 +0530

    FicoscoreCheckServiceApplicationTests Junit TestCase

--- src/test/java/com/mufg/ficoscorecheckservice/FicoscoreCheckServiceApplicationTests.java
+++ src/test/java/com/mufg/ficoscorecheckservice/FicoscoreCheckServiceApplicationTests.java
@@ -1,16 +1,53 @@
 package com.mufg.ficoscorecheckservice;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mufg.ficoscorecheckservice.model.FicoscoreCheckModel;
+import org.junit.Assert;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Sumalathav
+ *
+ */
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FicoscoreCheckServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext context;
+
+	ObjectMapper om = new ObjectMapper();
+
+	private FicoscoreCheckModel ficoscoreCheckModel;
+
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
 	}
 
+	@Test
+	public void getFicoscoreCheckTest() throws Exception {
+		MvcResult result = mockMvc.perform(
+				get("http://localhost:8091/mufg/api/ficoscorecheck/ssn/10").content(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk()).andReturn();
+		String resultContent = result.getResponse().getContentAsString();
+		ficoscoreCheckModel = om.readValue(resultContent, FicoscoreCheckModel.class);
+		Assert.assertTrue(ficoscoreCheckModel.isStatus() == Boolean.TRUE);
+
+	}
 }
